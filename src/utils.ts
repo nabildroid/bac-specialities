13f042efa1ebb570f206d32918a52b353d5392f7
nabildroid <pni20156789@gmail.com>
2022-07-12 12:37:47 +0100

    helper functions

---
+++ src/utils.ts
@@ -0,0 +1,20 @@
+import { Option } from "./models";
+
+export function calcDistance(a: number[], b: number[]) {
+  if (a.length != b.length)
+    throw new Error("the demensions are not the same between A, B");
+
+  const sum = a.reduce((acc, _, i) => acc + Math.pow(a[i] - b[i], 2), 0);
+
+  const distance = Math.sqrt(sum);
+
+  return distance;
+}
+
+export function OptionName(option: Option) {
+  return option.path.split("/")[1];
+}
+
+export function OptionType(option: Option) {
+  return option.path.split("/")[0];
+}
